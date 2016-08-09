becbc9da9d24341fbf76c4ac979ee8cde80c0728
Sougata Bhattacharjee <sougata.bhattacharjee@hotmail.com>
2016-08-09 08:40:53 +0200

    first commit

---
+++ src/main/java/com/de/stock/StockEvalApplication.java
@@ -0,0 +1,12 @@
+package com.de.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockEvalApplication.class, args);
+	}
+}
